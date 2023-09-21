3c45233b3598c902dfa39d770f27596ce1cef9bd
segars <119671020+segars@users.noreply.github.com>
2023-09-21 11:21:38 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Portafolio de sebastian Garces Puente</h1>
+      </header>
+      <main>
+        <div>
+        <h1>¿Quien soy?</h1>
+        
+        <p id='textito' >Hola me llamo sebastian, tengo 20 años y soy de contextura delgada. tengo cabello negro oscuro y ojos cafes oscuro que destacan en mi rostro. Suelo vestirme con ropa cómoda, como camisetas y jeans, pero también tengo un estilo un poco alternativo, me gusta la musica pero lamentable mente tengo una perdida de adudicion progresiva en mi oido izquiero lo que hacer que sea incomo escucharla, solia toca mi guitarra antes de que todo esto sucediera. En mis ratos libres, disfruta de practicar deportes como el basketball y me encanta pasar tiempo al aire libre como tambien me gusta jugar videojuegos casi todo el tiempo que estoy solo o sin planes de salir. soy un estudiante universitario, estudio desarrollo de software, y me interesa mucho comprender la mente humana y como se puede ayudar a personas con algunos problemas. Además, soy conocido por mo sentido del humor y mi capacidad para hacer reír a sus amigos. soy un chico amigable y sociable que valora las relaciones cercanas con su familia y amigos, y siempre está dispuesto a probar cosas nuevas y enfrentar desafíos.</p>
+        
+        <img id='sebas' src="sebas.jpg" alt="" />
+        </div>
+        <br></br>
+      </main>
+      <h1>Mis habilidades</h1>
+      <p> como mis habilidades tengo La capacidad para analizar problemas y desarrollar soluciones efectivas es fundamental en la programación, tambien tengo como habilidad poder desarrollar paginas web sencillas con funcionalidades basadas en Java script, tengo la habilidades de poder comunicar ideas a mi equipo de trabajo y buscar una forma de realizarlas de una forma mas estructurada y eficiente.</p>
+      <h1>Mis proyectos</h1>
+      <p>En mi vida como estudiante he desarrollado numerosos proyectos, pero los mas importantes son:</p>
+      <h1>1. Aplicación de Lista de Tareas</h1>
+      <p>Descripción: Una aplicación web que te permite crear, editar y eliminar tareas</p>
+      <img src="lista.jpg" alt="" />
+      <h1>2.Blog Personal</h1>
+      <p>Descripción: Un blog personal donde puedes escribir y publicar tus pensamientos y experiencias</p>
+      <img src="ja.jpg" alt="" />
+      <h1>3.Aplicación de Clima</h1>
+      <p>Descripción: Una aplicación que te proporciona información actualizada sobre el clima en diferentes ciudades.</p>
+      <img id='clim' src="clim.jpg" alt="" />
+
+      
+    </div>
+     
+      
+    
+  );
+  
+}
+
+
+export default App;
+  
